44372934e7f7c3ad865ea066858565dd1be1f894
Jeremy <jeremy@theknights.me>
2017-06-09 09:52:52 -0500

    added commander, daily_ap, monthly_ap, and wvw_rank json attributes from api

---
+++ DL.GuildWars2Api.Tests/DataTransferObjects/AccountTests.cs
@@ -0,0 +1,29 @@
+﻿using DL.GuildWars2Api.DataTransferObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DL.GuildWars2Api.Tests.DataTransferObjects
+{
+    [TestClass]
+    public class AccountTests
+    {
+        #region TotalAchievementPoints Property Tests
+
+        [TestMethod]
+        public void TotalAchievementPoints_Day0Month0_Total0()
+        {
+            var account = new Account { DailyAchievementPoints = 0, MonthlyAchievementPoints = 0 };
+            var actual = account.TotalAchievementPoints;
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void TotalAchievementPoints_Day1234Month4321_Total0()
+        {
+            var account = new Account { DailyAchievementPoints = 1234, MonthlyAchievementPoints = 4321 };
+            var actual = account.TotalAchievementPoints;
+            Assert.AreEqual(5555, actual);
+        }
+
+        #endregion
+    }
+}
--- DL.GuildWars2Api/DataTransferObjects/Account.cs
+++ DL.GuildWars2Api/DataTransferObjects/Account.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace DL.GuildWars2Api.DataTransferObjects
@@ -18,13 +19,32 @@ namespace DL.GuildWars2Api.DataTransferObjects
         [JsonProperty(PropertyName = "guilds")]
         public List<string> GuildIds { get; set; }
 
+        [JsonProperty(PropertyName = "guild_leader")]
+        public List<string> LeaderGuildIds { get; set; }
+
         [JsonProperty(PropertyName = "created")]
         public string Created { get; set; }
 
         [JsonProperty(PropertyName = "access")]
         public string Access { get; set; }
 
+        [JsonProperty(PropertyName = "commander")]
+        public bool HasCommanderTag { get; set; }
+
         [JsonProperty(PropertyName = "fractal_level")]
         public int FractalLevel { get; set; }
+
+        [JsonProperty(PropertyName = "daily_ap")]
+        public int DailyAchievementPoints { get; set; }
+
+        [JsonProperty(PropertyName = "monthly_ap")]
+        public int MonthlyAchievementPoints { get; set; }
+
+        [JsonProperty(PropertyName = "wvw_rank")]
+        public int WvwRank { get; set; }
+
+        public int TotalAchievementPoints => this.DailyAchievementPoints + this.MonthlyAchievementPoints;
+
+        public DateTime CreatedDate => DateTime.Parse(this.Created).ToLocalTime();
     }
 }
